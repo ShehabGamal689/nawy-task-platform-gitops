f7a6194a954c09b0aac46c59fcd93cb4a1130836
ShehabGamal689 <72823848+ShehabGamal689@users.noreply.github.com>
2026-05-20 01:35:30 +0300

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -12,7 +12,9 @@ fi
 read -p "Enter your S3 Bucket Name for Terraform State: " S3_BUCKET
 read -p "Enter your GitHub Username: " GITHUB_USERNAME
 read -sp "Enter your GitHub Personal Access Token: " GITHUB_TOKEN
-echo -e "\n✅ Credentials received."
+echo ""
+read -sp "Enter your New Relic License Key: " NR_LICENSE_KEY
+echo -e "\n Credentials received."
 echo "================================================="
 
 echo "Step 1: Terraform Apply..."
@@ -43,11 +45,14 @@ kubectl create namespace newrelic --dry-run=client -o yaml | kubectl apply -f -
 echo "Fetching GitHub token from AWS Secrets Manager..."
 FETCHED_TOKEN=$(aws secretsmanager get-secret-value --secret-id gitops-secrets-vault --query SecretString --output text --region $AWS_REGION)
 
-
 kubectl create secret generic github-token --from-literal=username=$GITHUB_USERNAME --from-literal=password=$FETCHED_TOKEN -n argocd --dry-run=client -o yaml | kubectl apply -f -
 kubectl create secret docker-registry ghcr-login --docker-server=ghcr.io --docker-username=ShehabGamal689 --docker-password=$FETCHED_TOKEN -n nawy-app --dry-run=client -o yaml | kubectl apply -f -
 
-echo "✅ Secrets successfully injected into the cluster."
+echo "Injecting New Relic Secrets..."
+kubectl create secret generic newrelic-key --from-literal=licenseKey=$NR_LICENSE_KEY -n newrelic --dry-run=client -o yaml | kubectl apply -f -
+kubectl create secret generic newrelic-apm-key --from-literal=NEW_RELIC_LICENSE_KEY=$NR_LICENSE_KEY -n nawy-app --dry-run=client -o yaml | kubectl apply -f -
+
+echo "Secrets successfully injected into the cluster."
 echo "================================================="
 
 
@@ -57,5 +62,5 @@ cd ansible
 ansible-playbook install-argocd.yaml
 
 echo "================================================="
-echo "🎉 DEPLOYMENT COMPLETE!"
+echo " DEPLOYMENT COMPLETE!"
 echo "To check Argo CD pod status, run: kubectl get pods -n argocd"
