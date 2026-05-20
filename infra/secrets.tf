c2b1ab74003d45dd433f56a5a422f809f2a13f1d
ShehabGamal689 <72823848+ShehabGamal689@users.noreply.github.com>
2026-05-20 05:30:00 +0300

    Delete infra directory

--- infra/secrets.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_secretsmanager_secret" "git_credentials" {
-  name                    = "gitops-secrets-vault"
-  recovery_window_in_days = 0 
-}
-
-resource "aws_secretsmanager_secret_version" "git_credentials_version" {
-  secret_id     = aws_secretsmanager_secret.git_credentials.id
-  secret_string = var.github_token
-}
