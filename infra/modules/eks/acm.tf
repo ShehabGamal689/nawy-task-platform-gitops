0f5d9fa4771ce0d51035f0271551687faeb33aee
ShehabGamal689 <72823848+ShehabGamal689@users.noreply.github.com>
2026-05-20 05:35:18 +0300

    Update acm.tf

--- infra/modules/eks/acm.tf
+++ infra/modules/eks/acm.tf
@@ -1,4 +1,3 @@
-# 1. Request the Certificate
 resource "aws_acm_certificate" "cert" {
   domain_name       = "devopsawy.site"
   validation_method = "DNS"
@@ -12,8 +11,6 @@ resource "aws_acm_certificate" "cert" {
   }
 }
 
-# 2. Create the DNS Record in Route 53 for Validation
-# This "proves" to AWS that you own the domain
 resource "aws_route53_record" "cert_validation" {
   for_each = {
     for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
@@ -31,13 +28,12 @@ resource "aws_route53_record" "cert_validation" {
   zone_id         = data.aws_route53_zone.selected.zone_id # Make sure you have a data source for your zone
 }
 
-# 3. The "Waiter" - This tells Terraform to wait until the cert is actually ready
 resource "aws_acm_certificate_validation" "cert" {
   certificate_arn         = aws_acm_certificate.cert.arn
   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
 }
 
-# Data source to find your existing Route 53 zone
+
 data "aws_route53_zone" "selected" {
   name         = "devopsawy.site"
   private_zone = false
