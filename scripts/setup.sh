18f6fc857e78ec85b82e6a6a412aa1d09fa96f1d
ShehabGamal689 <72823848+ShehabGamal689@users.noreply.github.com>
2026-05-30 23:50:22 +0300

    Add files via upload

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -11,16 +11,18 @@ fi
 
 read -p "Enter your S3 Bucket Name for Terraform State: " S3_BUCKET
 read -p "Enter your GitHub Username: " GITHUB_USERNAME
-read -sp "Enter your GitHub Personal Access Token: " GITHUB_TOKEN
+read -p "Enter your GitHub Personal Access Token: " GITHUB_TOKEN
 echo ""
-read -sp "Enter your New Relic License Key: " NR_LICENSE_KEY
+read -p "Enter your New Relic License Key: " NR_LICENSE_KEY
 echo ""
-read -sp "Enter your Domain Registrar API Key: " REGISTRAR_API_KEY
+read -p "Enter your Domain Registrar API Key: " REGISTRAR_API_KEY
 echo ""
 echo -e "\n Credentials received."
 echo "================================================="
 export TF_VAR_registrar_api_key=$REGISTRAR_API_KEY
 export TF_VAR_github_token=$GITHUB_TOKEN
+export TF_VAR_newrelic_key=$NR_LICENSE_KEY
+
 
 echo "Step 1: Terraform Apply..."
 cd infra
@@ -41,27 +43,7 @@ aws eks update-kubeconfig --region $AWS_REGION --name $CLUSTER_NAME
 
 echo "================================================="
 
-echo "Step 3: Kubernetes Secrets Setup..."
-
-kubectl create namespace argocd --dry-run=client -o yaml | kubectl apply -f -
-kubectl create namespace nawy-app --dry-run=client -o yaml | kubectl apply -f -
-kubectl create namespace newrelic --dry-run=client -o yaml | kubectl apply -f -
-
-echo "Fetching GitHub token from AWS Secrets Manager..."
-FETCHED_TOKEN=$(aws secretsmanager get-secret-value --secret-id gitops-secrets-vault --query SecretString --output text --region $AWS_REGION)
-
-kubectl create secret generic github-token --from-literal=username=$GITHUB_USERNAME --from-literal=password=$FETCHED_TOKEN -n argocd --dry-run=client -o yaml | kubectl apply -f -
-kubectl create secret docker-registry ghcr-login --docker-server=ghcr.io --docker-username=$GITHUB_USERNAME --docker-password=$FETCHED_TOKEN -n nawy-app --dry-run=client -o yaml | kubectl apply -f -
-
-echo "Injecting New Relic Secrets..."
-kubectl create secret generic newrelic-key --from-literal=licenseKey=$NR_LICENSE_KEY -n newrelic --dry-run=client -o yaml | kubectl apply -f -
-kubectl create secret generic newrelic-apm-key --from-literal=NEW_RELIC_LICENSE_KEY=$NR_LICENSE_KEY -n nawy-app --dry-run=client -o yaml | kubectl apply -f -
-
-echo "Secrets successfully injected into the cluster."
-echo "================================================="
-
-
-echo "Step 4: Running Ansible Bootstrapping..."
+echo "Step 3: Running Ansible Bootstrapping..."
 cd ansible
 
 ansible-playbook install-argocd.yaml
